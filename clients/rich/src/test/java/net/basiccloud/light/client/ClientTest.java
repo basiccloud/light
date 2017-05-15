a66309e51f0bd65d92f4a3f7690c8f85cdf2922a
andy <xieyanze@live.cn>
2017-05-15 11:41:49 +0800

    add direct test

--- clients/rich/src/test/java/net/basiccloud/light/client/ClientTest.java
+++ clients/rich/src/test/java/net/basiccloud/light/client/ClientTest.java
@@ -12,12 +12,22 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 public class ClientTest {
-    @Ignore("need server start")
+    @Ignore("need server start and etcd server")
     @Test
     public void test01() {
         PerformanceBaselineServiceBlockingStub client = GrpcClientBuilder.sync(
                 PerformanceBaselineServiceBlockingStub.class)
                 .protoClass(PerformanceBaselineProto.class).useRegistry("etcd://127.0.0.1").build();
+        EchoResponse echoResponse = client.echo(EchoRequest.newBuilder().setContent("aaaaa").build());
+        System.out.println(echoResponse);
+    }
+
+    @Ignore("need server start")
+    @Test
+    public void test4Direct() {
+        PerformanceBaselineServiceBlockingStub client = GrpcClientBuilder.sync(
+                PerformanceBaselineServiceBlockingStub.class)
+                .protoClass(PerformanceBaselineProto.class).useDirectAddress("127.0.0.1:1080").build();
         EchoResponse echoResponse = client.echo(EchoRequest.newBuilder().setContent("aaaaa").build());
         System.out.println(echoResponse);
     }
